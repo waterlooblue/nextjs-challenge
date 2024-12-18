c3dc7b3feb1bb2def4c00fbfc1b39013aca71924
Christopher Norkett <chris@waterlooblue.com>
2024-12-18 12:03:20 -0500

    Add Footer component to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import { HeaderModel } from "@/models/Header";
 import { FooterModel } from "@/models/Footer";
 
@@ -9,13 +10,13 @@ interface LayoutProps {
   children: ReactNode;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, header }) => {
+const Layout: React.FC<LayoutProps> = ({ children, header, footer }) => {
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen bg-[#f6f8fa]">
       <Header data={header}/>
       <main>{children}</main>
-      <footer/>
-    </>
+      <Footer data={footer}/>
+    </div>
   );
 };
 
