b3d0c15f3a68242670b22c2a26a375dcaec693af
Christopher Norkett <chris@waterlooblue.com>
2024-12-18 05:04:50 -0500

    Merge queries for page data and layout data

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,61 +1,45 @@
 import { gql } from "@apollo/client";
 
-export const GET_PAGE = {
-  query: gql`
-    query GetLandingPage {
-      pageCollection (limit: 1) {
-        items {
-          title
-          subtitle
-          heroImage {
-            url
-          }
-          heroBackground {
-            url
-          }
-          featureBackground {
-            url
-          }
-          featuresCollection {
-            items {
-              title
-              description
-              image {
-                url
-              }
+export const GET_PAGE = gql`
+  query GetLandingPage {
+    headerCollection (limit: 1) {
+      items {
+        logo {
+          url
+        }
+      }
+    }
+    pageCollection (limit: 1) {
+      items {
+        title
+        subtitle
+        heroImage {
+          url
+        }
+        heroBackground {
+          url
+        }
+        featureBackground {
+          url
+        }
+        featuresCollection {
+          items {
+            title
+            description
+            image {
+              url
             }
           }
         }
       }
     }
-  `
-}
-
-export const GET_HEADER = {
-  query: gql`
-    query GetHeader {
-      headerCollection (limit: 1) {
-        items {
-          logo {
-            url
-          }
+    footerCollection (limit: 1) {
+      items {
+        copyright
+        background {
+          url
         }
       }
     }
-  `
-}
-
-export const GET_FOOTER = {
-  query: gql`
-    query GetHeader {
-      footerCollection (limit: 1) {
-        items {
-          copyright
-          background {
-            url
-          }
-        }
-      }
-    }
-  `
-}
+  }
+`
