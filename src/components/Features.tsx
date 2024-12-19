80a6e584e5dac1ba5e1a7d4618c69ac4112f7b43
Christopher Norkett <chris@waterlooblue.com>
2024-12-18 19:10:32 -0500

    Add features component

---
+++ src/components/Features.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { PageModel } from "@/models/Page";
+
+interface Props {
+  data?: PageModel;
+}
+
+const Features = ({ data }: Props) => {
+  const { 
+    featuresCollection,
+    featureBackground
+  } = data || {};
+  console.log(featuresCollection)
+  return (
+    <div className="relative px-5 mb-[200px]">
+      <div className="grid md:grid-cols-2 gap-16 max-w-[700px] mx-auto">
+        {featuresCollection?.items?.map(feature => (
+          <div key={feature._id} className="flex flex-col flex-wrap items-center mx-auto max-w-[350px] relative z-10">
+            <Image
+              className="bg-white rounded-full shadow-lg mb-5"
+              src={feature.image?.url || ''}
+              width={feature.image?.width}
+              height={feature.image?.height}
+              alt=''
+            />
+            <h2 className="text-[#202b36] text-2xl font-bold mb-3">{feature.title}</h2>
+            <p className="text-[#5b6f82] text-lg text-center">{feature.description}</p>          
+          </div>
+        ))}
+      </div>
+      <Image
+        className="absolute -z-0 -bottom-[110px] left-0 md:left-[56%] min-w-[1205px]"
+        src={featureBackground?.url || ''}
+        width={featureBackground?.width}
+        height={featureBackground?.height}
+        alt=''
+      />
+    </div>
+  );
+};
+
+export default Features;
