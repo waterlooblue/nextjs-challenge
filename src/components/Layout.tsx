8338da25b696e37dbcbbc7fdacb97b12eaeb419e
Christopher Norkett <chris@waterlooblue.com>
2024-12-19 02:42:09 -0500

    Set fonts with layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { HeaderModel } from "@/models/Header";
 import { FooterModel } from "@/models/Footer";
+import { Roboto, Lato } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  weight: "700",
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  variable: "--font-lato",
+  weight: "400",
+});
 
 interface LayoutProps {
   header: HeaderModel;
@@ -12,7 +25,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children, header, footer }) => {
   return (
-    <div className="overflow-hidden xl:container flex flex-col justify-between min-h-screen bg-white shadow-xl">
+    <div className={`${roboto.variable} ${lato.variable} overflow-hidden xl:container flex flex-col justify-between min-h-screen bg-white shadow-xl`}>
       <Header data={header}/>
       <main className="grow">{children}</main>
       <Footer data={footer}/>
