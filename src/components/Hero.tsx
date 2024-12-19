c966f0c813c8ab90738431089cee7737d48758fa
Christopher Norkett <chris@waterlooblue.com>
2024-12-19 01:14:52 -0500

    Convert to form and add validation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { PageModel } from "@/models/Page";
@@ -7,6 +7,10 @@ import { validateEmail } from "@/util/disify";
 interface Props {
   data?: PageModel;
 }
+
+type EmailFormInputs = {
+  email: string;
+};
 
 const Hero = ({ data }: Props) => {
   const { 
@@ -17,19 +21,20 @@ const Hero = ({ data }: Props) => {
     heroBackground,
     heroImage
   } = data || {};
-  const [email, setEmail] = useState('');
+  const { register, formState: { errors, isValid }, handleSubmit } = useForm<EmailFormInputs>();
   const router = useRouter()
-  const handleClick = async () => {
-    const validation = await validateEmail(email);
-    console.log(validation)
-    if (validation.disposable) {
-      alert(`REQUEST FAILED: ${email} is invalid!`)
-    } else if (!validation.disposable && validation.format) {
-      router.push('/success');
+  const onSubmit: SubmitHandler<EmailFormInputs> = async (data) => {
+    if (isValid) {
+      const validation = await validateEmail(data.email);
+      if (validation.disposable) {
+        alert(`REQUEST FAILED: ${data.email} is invalid!`)
+      } else if (!validation.disposable && validation.format) {
+        router.push('/success');
+      }
     }
   };
   return (
-    <div className="relative px-5 text-center pb-5 mb-5">
+    <section className="relative px-5 text-center pb-5 mb-5">
       <Image
         className="absolute -z-0 -top-[25%] left-[20%] min-w-[1480px]"
         src={heroBackground?.url || ''}
@@ -39,20 +44,31 @@ const Hero = ({ data }: Props) => {
       />
       <h1 className="relative text-3xl md:text-4xl text-[#202b36] font-bold mb-8 z-10">{title}</h1>
       <p className="relative text-lg text-[#5b6f82] font-medium px-5 mx-auto max-w-[600px] mb-8 z-10">{subtitle}</p>
-      <div className="relative z-10 mb-[75px]">
-        <input 
-          placeholder={inputLabel}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="min-w-[250px] placeholder-gray-300 shadow appearance-none border focus:outline-none focus:shadow-outline py-3 px-3 mx-1 mb-3 lg:mb-0 w-full sm:w-[initial] rounded"
-        />
+      <form noValidate onSubmit={handleSubmit(onSubmit)} className="relative z-10 mb-[75px]">
+        <label htmlFor='email' className='relative block sm:inline-block'>
+          <input 
+            id='email'
+            type='email'
+            placeholder={inputLabel}
+            aria-invalid={!!errors.email}
+            className="min-w-[250px] placeholder-gray-300 shadow appearance-none border invalid:border-red-500 invalid:focus:ring-red-500 focus:outline-none focus:shadow-outline py-3 px-3 mx-1 mb-3 sm:mb-0 w-full sm:w-[initial] rounded"
+            required
+            {...register('email', { required: 'An E-mail address is required', pattern: /\S+@\S+\.\S+/ })}
+          />
+          {(errors.email && errors.email.type === 'required') &&
+            <p className='absolute text-red-500 -bottom-[85px] sm:-bottom-[30px] left-[8px]'>This field is required</p>
+          }
+          {(errors.email && errors.email.type === 'pattern') &&
+            <p className='absolute text-red-500 -bottom-[85px] sm:-bottom-[30px] left-[8px]'>Invalid email address</p>
+          }
+        </label>
         <button
-          onClick={handleClick}
+          type='submit'
           className="bg-gradient-to-r from-[#1676ed] to-[#4590f0] hover:from-blue-700 hover:to-blue-600 text-white text-xs font-bold uppercase py-4 px-5 mx-1 w-full sm:w-[initial] rounded"
         >
           {buttonCopy}
         </button>
-      </div>
+      </form>
       <div className="flex flex-wrap justify-center mb-12">
         <div className="relative mb-[80px]">
           <Image
@@ -92,7 +108,7 @@ const Hero = ({ data }: Props) => {
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
