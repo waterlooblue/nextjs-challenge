217300bfc8552864268fd9c6827a1db92e2f576b
Christopher Norkett <chris@waterlooblue.com>
2024-12-26 03:48:20 -0500

    Move success page into folder for app router

---
+++ src/app/success/page.tsx
@@ -0,0 +1,10 @@
+const Success = () => {
+  return (
+    <main className="grow">
+      <h1 className="relative text-3xl md:text-4xl text-[#202b36] font-bold mb-8 text-center z-10">{'Success!'}</h1>
+      <p className="relative text-lg text-[#5b6f82] font-medium px-5 mx-auto max-w-[600px] text-center mb-8 z-10">{'You have successfully signed up to our mailing list!'}</p>
+    </main>
+  );
+};
+
+export default Success;
